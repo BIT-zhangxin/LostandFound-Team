3a8ee244af5306dc714bdc1674e1e97a1ab639da
[BIT-zhangxin] <1047632321@qq.com>
2019-05-23 23:17:01 +0800

    上传图片功能
    from 张歆
    目前只能进行拍照，选择图片，裁剪；
    还没有存储进数据库中，所有的文件都是存在手机本地的

---
+++ app/src/main/java/com/example/lostandfound/activity/TestPhotoActivity.java
@@ -0,0 +1,220 @@
+package com.example.lostandfound.activity;
+
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.support.annotation.NonNull;
+import android.support.v4.content.FileProvider;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+import com.bumptech.glide.Glide;
+import com.example.lostandfound.R;
+import com.example.lostandfound.component.MyAppCompatActivity;
+import java.io.File;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Objects;
+
+public class TestPhotoActivity extends MyAppCompatActivity implements OnClickListener {
+
+    public static final int PHOTO_REQUEST_CODE=1;//拍照
+    public static final int PICTURE_SELECT_REQUEST_CODE =2;//选择图片
+
+    public static final int CROP_REQUEST_CODE=3;
+
+    public static final String FORMAT_JPG=".jpg";
+    public static final String FORMAT_JPEG=".jpeg";
+
+    private Uri imgUri; // 拍照时返回的uri
+    private Uri mCutUri;// 图片裁剪时返回的uri
+    private File imgFile;// 拍照保存的图片文件
+
+    private Button button1;
+    private Button button2;
+
+    private ImageView smallImg;
+
+    private boolean hasPermission = false;
+    private static final int REQUEST_PERMISSION = 100;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.test_photo_layout);
+        initView();
+        checkPermissions();
+    }
+
+    protected void initView(){
+        smallImg = findViewById(R.id.small_img);
+        button1= findViewById(R.id.btn_test_photo_photo);
+        button2= findViewById(R.id.btn_test_photo_select);
+        button1.setOnClickListener(this);
+        button2.setOnClickListener(this);
+    }
+
+    private void checkPermissions() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            // 检查是否有存储和拍照权限
+            if (checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED
+                && checkSelfPermission(Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED
+                ) {
+                hasPermission = true;
+            } else {
+                requestPermissions(new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.CAMERA}, REQUEST_PERMISSION);
+            }
+        }
+    }
+
+
+
+    @Override
+    public void onClick(View v){
+        switch(v.getId()){
+            case R.id.btn_test_photo_photo:
+                goCamera();
+                break;
+            case R.id.btn_test_photo_select:
+                goPictureAlbum();
+                break;
+            default:
+                break;
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == REQUEST_PERMISSION) {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                hasPermission = true;
+            } else {
+                Toast.makeText(this, "权限授予失败！", Toast.LENGTH_SHORT).show();
+                hasPermission = false;
+            }
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode,int resultCode,Intent data){
+        super.onActivityResult(requestCode,resultCode,data);
+
+        //如果图片为空
+        if(resultCode!=RESULT_OK) return;
+
+        //拍照
+        if(requestCode==PHOTO_REQUEST_CODE){
+            startPhotoZoom(imgUri,true);
+        }
+
+        //选择相册图片
+        else if(requestCode==PICTURE_SELECT_REQUEST_CODE){
+            startPhotoZoom(data.getData(),false);
+        }
+
+        //裁剪图片
+        else if(requestCode==CROP_REQUEST_CODE){
+            Glide.with(this).load(mCutUri).into(smallImg);
+        }
+    }
+
+    private void startPhotoZoom(Uri uri,boolean fromCamera){
+        Intent intent = new Intent("com.android.camera.action.CROP");
+
+        intent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+        intent.addFlags(Intent.FLAG_GRANT_WRITE_URI_PERMISSION);
+
+        // 设置裁剪
+        intent.setDataAndType(uri, "image/*");
+        intent.putExtra("scale", true);
+
+        //取消人脸识别
+        intent.putExtra("noFaceDetection", false);
+
+        //aspectX aspectY 是宽高的比例
+        intent.putExtra("aspectX", 1);
+        intent.putExtra("aspectY", 1);
+
+        //outputX outputY 是裁剪图片宽高
+        intent.putExtra("outputX", 250);
+        intent.putExtra("outputY", 250);
+
+        if(fromCamera){
+            //如果是使用拍照，那么原先的uri和最终目标的uri一致，注意这里的uri必须是Uri.fromFile生成的
+            File photoFile=createFile(FORMAT_JPEG);
+            mCutUri=Uri.fromFile(photoFile);
+        }
+        else{
+            //从相册中选择，那么裁剪的图片要新创建
+            File mCutFile=createFile(FORMAT_JPEG);
+            mCutUri=Uri.fromFile(mCutFile);
+        }
+
+        intent.putExtra("return-data", false);
+        intent.putExtra("output", mCutUri);
+        intent.putExtra("outputFormat", Bitmap.CompressFormat.JPEG.toString());// 图片格式
+
+        startActivityForResult(intent, CROP_REQUEST_CODE);//这里就将裁剪后的图片的Uri返回了
+    }
+
+    //调用相机
+    private void goCamera(){
+        Intent intent=new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+
+        imgFile=createFile(FORMAT_JPG);
+        imgUri=getUriForFile(this, imgFile);
+
+        intent.putExtra(MediaStore.EXTRA_OUTPUT,imgUri);
+
+
+
+        startActivityForResult(intent,PHOTO_REQUEST_CODE);
+    }
+
+    //调用系统的相册
+    private void goPictureAlbum(){
+        Intent intent=new Intent(Intent.ACTION_PICK);
+        intent.setType("image/*");
+
+        startActivityForResult(intent,PICTURE_SELECT_REQUEST_CODE);
+    }
+
+    //获取文件uri(android7.0后必须使用FileProvider)
+    private Uri getUriForFile(Context context,File file) {
+        Uri uri;
+        if (Build.VERSION.SDK_INT>=24) {
+            uri=FileProvider.getUriForFile(context,"com.example.lostandfound.fileprovider",file);
+        } else {
+            uri=Uri.fromFile(file);
+        }
+        return uri;
+    }
+
+    //创建文件
+    private File createFile(String format){
+        String storagePath=Objects.requireNonNull(getApplication().getExternalFilesDir(Environment.DIRECTORY_PICTURES)).getAbsolutePath();
+        File storageDir=new File(storagePath);
+        //noinspection ResultOfMethodCallIgnored
+        storageDir.mkdirs();
+        return new File(storageDir,getStringToday()+format);
+    }
+
+    //获取当前时间字符串
+    @SuppressLint("SimpleDateFormat")
+    private String getStringToday(){
+        Date currentTime=new Date();
+        SimpleDateFormat formatter=new SimpleDateFormat("yyyyMMddHHmmss");
+        return formatter.format(currentTime);
+    }
+}
